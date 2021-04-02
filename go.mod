87cc6ecc599ee24414f37817acaf019aa9808791
betates <rofiq.beta@gmail.com>
2021-04-03 04:55:13 +0700

    Belajar Aplikasi Module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/betates/app-go
+
+go 1.16
+
+require github.com/betates/go-module/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_app "github.com/betates/go-module/v2"
+)
+
+func main() {
+	fmt.Println(go_app.SayHello("Rofiq"))
+}
